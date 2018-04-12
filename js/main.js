f7b2eed7e50f1a3a202de4c7996e4ca7e4a66be4
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-04-12 11:41:34 +0200

    add code change

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 $(function(){
     //this code will execute after the DOM is loaded
-    var carouselList = $('#carousel ul');
-    setInterval(changeSlide, 3000);
+    var $carouselList = $('#carousel ul');
+    var $buttonRight = $('.btn-right');
+    var $buttonLeft = $('.btn-left');
+    //setInterval(changeSlide, 3000);
     function changeSlide() {
-        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+        $carouselList.animate({'marginLeft':-800}, 500, moveFirstSlide);
     }
     function moveFirstSlide() {
-        var firstItem = carouselList.find('li:first');
-        var lastItem = carouselList.find('li:last');
+        var firstItem = $carouselList.find('li:first');
+        var lastItem = $carouselList.find('li:last');
         lastItem.after(firstItem)
-        carouselList.css({marginLeft:0});
+        $carouselList.css({marginLeft:-400});
     }
+    function changeSlideLeft() {
+        $carouselList.animate({'marginLeft':0}, 500, moveLastSlide);
+    }
+    function moveLastSlide() {
+        var firstItem = $carouselList.find('li:first');
+        var lastItem = $carouselList.find('li:last');
+        firstItem.before(lastItem)
+        $carouselList.css({marginLeft:-400});
+    }
+    $buttonRight.on('click', function() {
+        changeSlide();
+    })
+    $buttonLeft.on('click', function() {
+        changeSlideLeft();
+    })
+    
+
 });
